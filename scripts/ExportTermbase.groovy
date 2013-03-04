974808f8105b3fea718af2954bbb9f22b51d37bb
Stephane AUBRY <saubry@japotrad.com>
2013-03-04 21:15:21 +0700

    Rename tmf package into pangloss.tmf Create tmf.xsl package for XslFileFilterFactory class.

--- scripts/ExportTermbase.groovy
+++ scripts/ExportTermbase.groovy
@@ -1,12 +1,12 @@
 // @ExecutionModes({ON_SINGLE_NODE})
-import tmf.Tdc
-import tmf.Config
+import pangloss.xsl.XslFileFilterFactory
+import pangloss.tmf.Tdc
+import pangloss.tmf.Config
 
 import org.freeplane.core.ui.components.UITools
 import org.freeplane.core.ui.ExampleFileFilter
 import org.freeplane.core.util.LogUtils
 import org.freeplane.core.util.TextUtils
-import org.freeplane.core.util.LogUtils
 
 import org.freeplane.features.export.mindmapmode.IExportEngine
 import org.freeplane.features.export.mindmapmode.XsltExportEngine
@@ -18,7 +18,6 @@ import groovy.util.ConfigSlurper
 import java.beans.PropertyChangeEvent
 import java.beans.PropertyChangeListener
 
-import javax.swing.filechooser.FileFilter
 import javax.swing.filechooser.FileSystemView
 import javax.swing.JFileChooser
 import javax.swing.BorderFactory
@@ -32,8 +31,6 @@ import java.io.File
 import java.io.FileReader
 import java.io.IOException
 
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 import java.util.HashMap
 import java.util.List
 
@@ -43,139 +40,6 @@ import javax.xml.transform.stream.StreamSource
 import javax.xml.transform.OutputKeys
 import javax.xml.transform.Transformer
 
-
-/**
- * XslFileFilterFactory handles the file filters of the file chooser dialog.
- * There are two kinds of filters:<ul>
- * <li>The filters genuinely embedded in the program, like the GMT filter.</li>
- * <li>The filters defined externally in .xsl stylesheets and discovered at runtime.</li></ul>
- * This class is inspired from XsltExportEngineFactory.java and ExportController.java of Freeplane source code.
- * @author Stephane Aubry
- */
-private class XslFileFilterFactory {
-	/**
-	 * <code>xslFilterMap</code> is a map of file filters defined externally in .xsl stylesheets. Keys of the map entries are filters. Values of the map entries are the stylesheet files.
-	 */
-	Map<FileFilter, File> xslFilterMap = new HashMap<ExampleFileFilter, File>()
-	
-	/**
-	 * <code>fileFilters</code> is the complete list of file filters. It contains both internal and external filters.
-	 */
-	List<ExampleFileFilter> fileFilters = new ArrayList<ExampleFileFilter>()
-	
-	/**
-	 * COMPILED_EXPORT_FILTER_PATTERN should match in the first lines of a valid .xsl stylesheet.
-	// Example: <!-- 	TERMBASEEXPORTFILTER csv;tsv 2-col Spreadsheet -->
-	*/
-	Pattern COMPILED_EXPORT_FILTER_PATTERN = Pattern.compile('^.*TERMBASEEXPORTFILTER\\s+(\\S+)\\s+(.*)(?:\\s+-->)?$')
-	/**
-	 * Creates the factory and define the filters genuinely embedded in the program, like the GMT filter.
-	 * @return The new <code>XslFileFilterFactory</code> object.
-	 */
-	public XslFileFilterFactory(){
-		fileFilters.add(new ExampleFileFilter(['gmt'] as String[], TextUtils.getOptionalTranslation('Generic Mapping Tool')))
-		fileFilters.add(new ExampleFileFilter(['csv'] as String[], TextUtils.getOptionalTranslation('Comma-separated Value')))
-		fileFilters.add(new ExampleFileFilter(['tsv'] as String[], TextUtils.getOptionalTranslation('Tab-separated Value')))
-	}
-	
-	/** Returns <code>xslFilterMap</code>, a map of file filters defined externally in .xsl stylesheets. Keys of the map entries are filters. Values of the map entries are the stylesheet files.
-	* @return The <code>xslFilterMap</code> map.
-	*/
-	public HashMap<FileFilter, File> getFilterMap() {
-		return xslFilterMap
-	}
-	/** Returns <code>fileFilters</code>, the complete list of file filters, containing both internal and external filters.
-	 * @return The <code>fileFilters</code> list.
-	 */
-	 public List<ExampleFileFilter> getFileFilters() {
-		 return fileFilters
-	 }
-	/** Adds an externally defined filter.
-	 * @param extensions The semicolon separated list of file extensions (without the period character) supported by the filter. Example: 'csv;tsv'
-	 * @param description A string describing the filter. Example: '2-col Spreadsheet'
-	 * @param xslFile The xsl stylesheet implementing the filter transformation from the GMT format.
-	 */
-	void addXsltFile(List<String> extensions, String description, File xslFile) {
-		ExampleFileFilter filter=new ExampleFileFilter(extensions as String[], TextUtils.getOptionalTranslation(description))
-		fileFilters.add(filter)
-		xslFilterMap.put(filter, xslFile)
-	}
-
-	/**
-	* Checks all readable files ending in '.xsl' from a given directory,
-	* and passes them to the method {@link #extractFilterFromFile}.
-	* @param xsltDir The directory where xsl stylesheet files are to be searched for.
-	*/
-	private void gatherXsltScripts(File xsltDir) {
-		List<File> xslFiles = new ArrayList<File>()
-		if (!(xsltDir.isDirectory() && xsltDir.canRead())) {
-			LogUtils.info('ExportTermbase	There is a problem related to the folder containing the xsl stylesheet conversion files (from GMT format): '+ xsltDir.getPath())
-			return
-		}
-		// we list the files using an anonymous filter class that accepts only files
-		// readable by the user and with name ending in .xsl
-		xslFiles = xsltDir.listFiles(new java.io.FileFilter() {
-			public boolean accept(final File pathname) {
-				return (pathname.isFile() && pathname.canRead() && pathname.getPath().toLowerCase().endsWith(".xsl"))
-			}
-		})
-		// For each found file, we check and extract a potentially present filter
-		for (int i = 0; i < xslFiles.size(); i++) {
-			extractFilterFromFile(xslFiles[i]);
-		}
-	}
-	
-	/**
-	 * Checks if the COMPILED_EXPORT_FILTER_PATTERN match within the first 5 lines of the file.
-	 * @param xsltFile The file where the pattern is to be searched for.
-	 */
-	void extractFilterFromFile(final File xsltFile) {
-		/** Maximum number of lines we read in each XSLT files for performance reasons */
-		int MAX_READ_LINES = 5
-		BufferedReader xsl = null
-		try {
-			xsl = new BufferedReader(new FileReader(xsltFile))
-			String line
-			int l = 0
-			boolean keyFound = false
-			// ...we open it and check if it contains the right marker
-			while ((line = xsl.readLine()) != null && l < MAX_READ_LINES) {
-				final Matcher m = COMPILED_EXPORT_FILTER_PATTERN.matcher(line)
-				if (m.matches()) { // if it does
-					keyFound = true
-					List<String> extensions = m.group(1).split("\\s*;\\s*")
-					String description = m.group(2).trim()
-					if(description.startsWith("%")){
-						description = TextUtils.getText(description.substring(1))
-					}
-					addXsltFile(extensions, description, xsltFile);
-					// we want to allow for more than one filter line per XSLT file
-					// so we don't exit once we've found one and even account for
-					// the fact that we might trespass the MAX_READ_LINES limit
-					l--;
-				}
-				l++;
-			}
-			if (keyFound) {
-				return;
-			}
-		}
-		catch (final IOException e) {
-			LogUtils.warn(e);
-			UITools.errorMessage(TextUtils.getText("export_failed"));
-		}
-		finally {
-			if (xsl != null) {
-				try {
-					xsl.close();
-				}
-				catch (final IOException e) {
-					LogUtils.severe(e);
-				}
-			}
-		}
-	}
-}
 
 /**
  * A FileChooserAccessory defines the GUI to the right of the file viewer panel of the file chooser dialog.
