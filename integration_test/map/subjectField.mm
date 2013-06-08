d1ba65c08ec88f42191309e548d3d7de04aafaab
Stephane AUBRY <saubry@japotrad.com>
2013-06-08 11:30:56 +0700

    #10

---
+++ integration_test/map/subjectField.mm
@@ -0,0 +1,74 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="subjectField" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1370663829093"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Setting"/>
+<stylenode TEXT="Entry"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<attribute_layout NAME_WIDTH="96"/>
+<attribute NAME="subjectField" VALUE="economics"/>
+<node TEXT="cash flow" STYLE_REF="Entry" POSITION="right" ID="ID_472801883" CREATED="1370663900890" MODIFIED="1370664521312">
+<edge COLOR="#ff0000"/>
+<attribute NAME="subjectField" VALUE="+finance"/>
+<node TEXT="operting cash flow" STYLE_REF="Entry" ID="ID_1795268918" CREATED="1370664161437" MODIFIED="1370664521328"/>
+</node>
+<node TEXT="GDP" STYLE_REF="Entry" POSITION="right" ID="ID_775501300" CREATED="1370664156671" MODIFIED="1370664521343">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="dish" STYLE_REF="Entry" POSITION="left" ID="ID_738613972" CREATED="1370664042859" MODIFIED="1370664508312">
+<edge COLOR="#0000ff"/>
+<attribute NAME="subjectField" VALUE="food"/>
+<node TEXT="cake" STYLE_REF="Entry" ID="ID_12026309" CREATED="1370664111281" MODIFIED="1370664508328">
+<attribute NAME="subjectField" VALUE="+dessert"/>
+</node>
+</node>
+</node>
+</map>
