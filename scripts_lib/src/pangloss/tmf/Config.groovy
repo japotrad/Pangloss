b5afc8edc3ad1bb92fb2f540b5dcf70d738487e5
Stephane AUBRY <saubry@japotrad.com>
2013-06-08 11:23:21 +0700

    Issue #10

--- scripts_lib/src/pangloss/tmf/Config.groovy
+++ scripts_lib/src/pangloss/tmf/Config.groovy
@@ -1,18 +1,21 @@
 package pangloss.tmf
 
 import java.util.Map
+
+import org.freeplane.plugin.script.proxy.Proxy
+
 /**
  * The Config class statically contains the configuration parameters used by the classes of the tmf package.
  * @author Stephane Aubry
  */
 class Config {
 	/**
-	 * A map of configuration values. Here is the lis of configuration parameters:<ul>
-	 * <li><code>pangloss_field_separator</code>: String to be inserted as a separator when concatenating the value of the <code>subjectField<code> Information Unit.</li>
+	 * A map of configuration values. Here is the list of mandatory configuration parameters:<ul>
+	 * <li><code>pangloss_field_separator</code>: Separator between a subjectField and its sub-fields. Fix value: &gt; </li>
 	 * </ul>
 	 * @see tmf.Iu
 	 */
-	public static Map<String,String> config = new HashMap<String,String>()
+	public static Map<String,String> config =['pangloss_field_separator':' > ']
 	/**
 	 * Adds a parameter in the config map.
 	 * @param key The parameter key
@@ -31,5 +34,27 @@ class Config {
 	 */
 	def static get(String key) {
 		return config[key]
+	}
+
+	/**
+	 * Browse the siblings of the node having style=Setting and update the config map accordingly.
+	 * @param node The map node
+	 */
+	def static refresh(Proxy.Node node) {
+		if (node.isRoot()) {return}
+		def parentNode=node.parent
+		parentNode.children.each {
+			def siblingNode = it
+			if (siblingNode.style.name=="Setting"){
+				if (siblingNode.plainText=="Parameters"){
+					for (int i in 0..siblingNode.attributes.size()-1) {
+						def attributeName = siblingNode.attributes.getNames()[i]
+						if (attributeName != "pangloss_field_separator"){ // Constant parameters should not be updated.
+							Config.set(attributeName, siblingNode.attributes.getValues()[i].toString())
+						}
+					}
+				}
+			}
+		}	
 	}
 }
